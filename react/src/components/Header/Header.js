5702ca0eda24f829de3a356e533e1ecfd1a23eac
YuLim2 <jyulim0120@gmail.com>
2022-07-14 19:43:16 +0900

    ADD: search icon and hambug bar

--- react/src/components/Header/Header.js
+++ react/src/components/Header/Header.js
@@ -1,9 +1,32 @@
+import imgA from './icons/menu.png';
+import imgB from './icons/search.png';
+import './Header.scss';
+
+import {useContext, useState} from "react";
+
 export default function Header(){
+
+    const [searchText, setSearchText] = useState("");
+    const [searchTextOnFocus, setSearchTextOnFocus] = useState(false);
+
+    const toggleSearchTextOnFocus = e => {
+        setSearchTextOnFocus(!searchTextOnFocus)
+    }
+
+    const writeSearchText = e => {
+        setSearchText(e.target.value)
+    }
+
+    const resetSearchText = e => {
+        setSearchText("");
+    }
+
     return (
         <header>
             <div className="header-top">
                 <div className="header-inner">
-                    헤더임다
+                    <img src={imgA} className="menu" />
+                    <img src={imgB} className="search" />
                 </div>
             </div>
         </header>
