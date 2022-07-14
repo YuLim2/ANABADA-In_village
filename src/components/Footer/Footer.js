090f4c484ee9c0d6650623783ff54b8bb118bb8a
YuLim2 <jyulim0120@gmail.com>
2022-07-14 11:23:02 +0900

    ADD: Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,29 @@
+import './Footer.scss';
+
+export default function Footer(){
+    return (
+        <footer>
+            <div className="footer-top">
+                <div className="footer-inner">
+                    <ul>
+                        <li className='object'>
+                            교환
+                        </li>
+                        <li className='object'>
+                            대여
+                        </li>
+                        <li className='object'>
+                            홈
+                        </li>
+                        <li className='object'>
+                            나눔
+                        </li>
+                        <li className='object'>
+                            프로필
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+export default function Header(){
+    return (
+        <header>
+            <div className="header-top">
+                <div className="header-inner">
+                    헤더임다
+                </div>
+            </div>
+        </header>
+    )
+}
